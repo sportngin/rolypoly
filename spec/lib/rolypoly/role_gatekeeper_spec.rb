3a59658fb2cb54ba52312d6963c6434bc96ea302
Jake Waletzko <jake.waletzko@sportngin.com>
2016-12-05 16:29:12 -0600

    add role resource for gatekeeper spec

--- spec/lib/rolypoly/role_gatekeeper_spec.rb
+++ spec/lib/rolypoly/role_gatekeeper_spec.rb
@@ -4,20 +4,21 @@ module Rolypoly
   describe RoleGatekeeper do
     let(:roles) { %w[admin scorekeeper] }
     let(:actions) { %w[index show] }
+    let(:role_resource) { [] }
 
-    subject { described_class.new roles, actions }
+    subject { described_class.new roles, actions, false }
 
     shared_examples_for "allow should behave correctly" do
       it "shouldn't auto-allow" do
-        expect(subject.allow?(nil, nil)).to be false
+        expect(subject.allow?(nil, nil, role_resource)).to be false
       end
 
       it "should allow scorekeepr access to index" do
-        expect(subject.allow?([:scorekeeper], "index")).to be true
+        expect(subject.allow?([:scorekeeper], "index", role_resource)).to be true
       end
 
       it "should not allow scorekeepr access to edit" do
-        expect(subject.allow?([:scorekeeper], "edit")).to be false
+        expect(subject.allow?([:scorekeeper], "edit", role_resource)).to be false
       end
 
       describe "all public" do
@@ -26,15 +27,15 @@ module Rolypoly
         end
 
         it "should allow whatever" do
-          expect(subject.allow?(nil, nil)).to be true
+          expect(subject.allow?(nil, nil, role_resource)).to be true
         end
 
         it "should allow scorekeepr access to index" do
-          expect(subject.allow?([:scorekeeper], "index")).to be true
+          expect(subject.allow?([:scorekeeper], "index", role_resource)).to be true
         end
 
         it "should allow scorekeepr access to edit" do
-          expect(subject.allow?([:scorekeeper], "edit")).to be true
+          expect(subject.allow?([:scorekeeper], "edit", role_resource)).to be true
         end
       end
 
@@ -44,17 +45,17 @@ module Rolypoly
         end
 
         it "shouldn't auto-allow" do
-          expect(subject.allow?(nil, nil)).to be false
+          expect(subject.allow?(nil, nil, role_resource)).to be false
         end
 
         it "should allow scorekeepr access to index" do
-          expect(subject.allow?([:janitor], "index")).to be true
-          expect(subject.allow?([:admin], "index")).to be true
+          expect(subject.allow?([:janitor], "index", role_resource)).to be true
+          expect(subject.allow?([:admin], "index", role_resource)).to be true
         end
 
         it "to should not allow scorekeepr access to edit" do
-          expect(subject.allow?([:scorekeeper], "edit")).to be false
-          expect(subject.allow?([:janitor], "edit")).to be false
+          expect(subject.allow?([:scorekeeper], "edit", role_resource)).to be false
+          expect(subject.allow?([:janitor], "edit", role_resource)).to be false
         end
       end
 
@@ -64,19 +65,19 @@ module Rolypoly
         end
 
         it "shouldn't auto-allow" do
-          expect(subject.allow?(nil, nil)).to be false
+          expect(subject.allow?(nil, nil, role_resource)).to be false
         end
 
         it "should allow scorekeepr access to index" do
-          expect(subject.allow?([:scorekeeper], "index")).to be true
+          expect(subject.allow?([:scorekeeper], "index", role_resource)).to be true
         end
 
         it "shouldn't allow janitor access to any" do
-          expect(subject.allow?([:janitor], "index")).to be false
+          expect(subject.allow?([:janitor], "index", role_resource)).to be false
         end
 
         it "should allow scorekeepr access to edit" do
-          expect(subject.allow?([:scorekeeper], "edit")).to be true
+          expect(subject.allow?([:scorekeeper], "edit", role_resource)).to be true
         end
       end
     end
@@ -94,6 +95,16 @@ module Rolypoly
 
     describe "with only actions set" do
       let(:roles) { [] }
+
+      before do
+        subject.to(:admin, :scorekeeper)
+      end
+
+      it_should_behave_like "allow should behave correctly"
+    end
+
+    describe "with role_resource defined" do
+      let(:role_resource) { [organization: 123] }
 
       before do
         subject.to(:admin, :scorekeeper)
