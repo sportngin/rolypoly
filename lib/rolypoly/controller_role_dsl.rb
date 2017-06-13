6540cc7e890209340b5711b079c700802da0762c
Jon Karna <ipg49vv2@gmail.com>
2017-06-13 01:28:58 -0500

    Need to include RoleDSL directly due to method creation

--- lib/rolypoly/controller_role_dsl.rb
+++ lib/rolypoly/controller_role_dsl.rb
@@ -4,8 +4,6 @@ module Rolypoly
   FailedRoleCheckError = Class.new StandardError
   module ControllerRoleDSL
 
-    include RoleDSL::InstanceMethods
-
     def self.included(sub)
       sub.before_filter(:rolypoly_check_role_access!) if sub.respond_to? :before_filter
       if sub.respond_to? :rescue_from
@@ -18,42 +16,44 @@ module Rolypoly
         end
       end
 
+      sub.send(:include, RoleDSL)
       sub.extend(ClassMethods)
+      sub.send(:include, InstanceMethods)
     end
 
-    def rolypoly_check_role_access!
-      failed_role_check! unless rolypoly_role_access?
-    end
+    module InstanceMethods
+      def rolypoly_check_role_access!
+        failed_role_check! unless rolypoly_role_access?
+      end
 
-    def failed_role_check!
-      raise Rolypoly::FailedRoleCheckError
-    end
+      def failed_role_check!
+        raise Rolypoly::FailedRoleCheckError
+      end
 
-    def current_roles
-      allowed_roles(action_name)
-    end
+      def current_roles
+        allowed_roles(action_name)
+      end
 
-    def public?
-      super(action_name)
-    end
+      def public?
+        super(action_name)
+      end
 
-    def current_gatekeepers
-      rolypoly_gatekeepers.select { |gatekeeper|
-        gatekeeper.action? action_name
-      }
-    end
+      def current_gatekeepers
+        rolypoly_gatekeepers.select { |gatekeeper|
+          gatekeeper.action? action_name
+        }
+      end
 
-    def allow?(options = {})
-      rolypoly_gatekeepers.allow?(current_roles, action_name, rolypoly_resource_map.merge(options))
-    end
+      def allow?(options = {})
+        rolypoly_gatekeepers.allow?(current_roles, action_name, rolypoly_resource_map.merge(options))
+      end
 
-    private def rolypoly_role_access?
-      allow?
+      private def rolypoly_role_access?
+        allow?
+      end
     end
 
     module ClassMethods
-      include RoleDSL::ClassMethods
-
       private def rolypoly_gatekeepers=(arry)
         @rolypoly_gatekeepers = Rolypoly::RoleGatekeepers.new(arry)
       end
